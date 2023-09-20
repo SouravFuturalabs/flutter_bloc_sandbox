3f1cdd1cdfcae7969f1e3b41310ca0f6e778087d
shanu <shanu.futura@gmail.com>
2023-09-20 16:56:40 +0530

    first commit

---
+++ lib/data/product_data.dart
@@ -0,0 +1,58 @@
+class ProductsData{
+   static List<Map<String, dynamic>> productList = [
+    {
+      'id': 1,
+      'name': 'Product 1',
+      'price': 19.99,
+      'description': 'This is the first product description.',
+      'rating': 4.5,
+      'inStock': true,
+      'image': 'https://example.com/product1.jpg',
+    },
+    {
+      'id': 2,
+      'name': 'Product 2',
+      'price': 29.99,
+      'description': 'This is the second product description.',
+      'rating': 3.8,
+      'inStock': false,
+      'image': 'https://example.com/product2.jpg',
+    },
+    {
+      'id': 3,
+      'name': 'Product 3',
+      'price': 39.99,
+      'description': 'This is the third product description.',
+      'rating': 4.0,
+      'inStock': true,
+      'image': 'https://example.com/product3.jpg',
+    },
+    {
+      'id': 4,
+      'name': 'Product 4',
+      'price': 49.99,
+      'description': 'This is the fourth product description.',
+      'rating': 4.2,
+      'inStock': true,
+      'image': 'https://example.com/product4.jpg',
+    },
+    {
+      'id': 5,
+      'name': 'Product 5',
+      'price': 59.99,
+      'description': 'This is the fifth product description.',
+      'rating': 4.7,
+      'inStock': false,
+      'image': 'https://example.com/product5.jpg',
+    },
+    {
+      'id': 6,
+      'name': 'Product 6',
+      'price': 69.99,
+      'description': 'This is the sixth product description.',
+      'rating': 4.9,
+      'inStock': true,
+      'image': 'https://example.com/product6.jpg',
+    },
+  ];
+}
